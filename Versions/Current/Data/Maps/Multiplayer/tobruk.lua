c0750f0299303de98832fab126c74475552a536e
Johannes Frank <jmfrank63@gmail.com>
2026-05-16 18:42:44 +0700

    Remove LFS and stop tracking compiled assets

--- Versions/Current/Data/Maps/Multiplayer/tobruk.lua
+++
@@ -1,2 +0,0 @@
-function Init()
-end;
--- Versions/Current/Data/UI/escapemenu/escapemenu.lua
+++
@@ -1,8 +0,0 @@
-function LuaProcessMessage( nMessageCode, nFirst, nSecond )
-	local nTemp = ProcessMessageWithLink( nMessageCode, nFirst );
-	if ( nTemp ~= 0 ) then
-		return 1
-	end
-
-return 0
-end
