2a8d322139ab731df6471b94b7d614980ce51a9b
Johannes Maria Frank <jmfrank63@gmail.com>
2026-08-03 12:00:00 +0700

    fix: establish Winsock2 include order

--- Sources/src/Net/StdAfx.h
+++ Sources/src/Net/StdAfx.h
@@ -14,6 +14,7 @@
 #define _NOTHREADS 1
 #if defined(_WIN32) || defined(_WIN64)
 #define WIN32_LEAN_AND_MEAN							// Exclude rarely-used stuff from Windows headers
+#include <winsock2.h>
 #endif
 #include <assert.h>
 #ifdef GetObject
