e016dca0624196231b9d8ecf63b4935fda901239
Johannes Maria Frank <jmfrank63@gmail.com>
2026-08-07 11:22:00 +0700

    fix: keep Linux game entry on libc policy

--- build.zig
+++ build.zig
@@ -2282,9 +2282,7 @@ fn addGame(
     switch (build_support.entryPoint(platform, true)) {
         .win_main_crt_startup => game.entry = .{ .symbol_name = "WinMainCRTStartup" },
         .main_crt_startup => game.entry = .{ .symbol_name = "mainCRTStartup" },
-        .main => {
-            if (platform == .linux_x64) game.entry = .{ .symbol_name = "_start" };
-        },
+        .main => {},
     }
     return game;
 }
